2120e4ec8c589ac0e301f72d34e209541f488915
Krzysztof Ignasiak <141998545+kkignasiak98@users.noreply.github.com>
2026-05-26 13:40:05 +0000

    feat: add initial version of nix package

---
+++ default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "container-diffoscope";
+  version = "0.1.0";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "kkignasiak98";
+    repo = "container-diffoscope";
+    tag = finalAttrs.version;
+    hash = "sha256-Ox5MfVzCQU8y+P3N5KqTEMSVLHeIaIhZlZdgLAdA2Mw=";
+  };
+
+  build-system = [
+    python3Packages.poetry-core
+  ];
+
+  dependencies = with python3Packages; [
+    polars
+    typer
+  ];
+
+  pythonImportsCheck = [
+    "container_diffoscope"
+  ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Elegant Container filesystem comparison";
+    homepage = "https://github.com/kkignasiak98/container-diffoscope";
+    changelog = "https://github.com/kkignasiak98/container-diffoscope/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit; 
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "container-diffoscope";
+  };
+})
