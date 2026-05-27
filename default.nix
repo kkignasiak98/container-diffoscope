fcd50868fb8584e13c14c742fe39904dab74c5dd
kkignasiak98 <kkignasiak.98@outlook.com>
2026-05-27 07:45:55 +0000

    feat: add initial version of nix package definition

---
+++ default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  nix-update-script,
+  diffoscope,
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "container-diffoscope";
+  version = "0.1.0";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "kkignasiak98";
+    repo = "container-diffoscope";
+    tag = finalAttrs.version;
+    hash = "sha256-Ox5MfVzCQU8y+P3N5KqTEMSVLHeIaIhZlZdgLAdA2Mw=";
+  };
+
+  build-system = [
+    python3Packages.poetry-core
+    python3Packages.pythonRelaxDepsHook
+  ];
+
+  pythonRelaxDeps = [ "polars" ];
+
+  dependencies = with python3Packages; [
+    polars
+    typer
+  ];
+
+  propagatedBuildInputs = [
+    diffoscope
+  ];
+
+  pythonImportsCheck = [
+    "container_diffoscope"
+  ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Elegant Container filesystem comparison";
+    homepage = "https://github.com/kkignasiak98/container-diffoscope";
+    changelog = "https://github.com/kkignasiak98/container-diffoscope/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit; 
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "container-diffoscope";
+  };
+})
