11d8fb1ae99960157a6105466007862cbc139402
Krzysztof Ignasiak <141998545+kkignasiak98@users.noreply.github.com>
2026-06-01 11:33:29 +0200

    feat: update default.nix to work with the newest version (#14)

--- default.nix
+++ default.nix
@@ -8,19 +8,20 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "container-diffoscope";
-  version = "0.1.0";
+  version = "0.2.0";
   pyproject = true;
   __structuredAttrs = true;
 
-  src = fetchFromGitHub {
-    owner = "kkignasiak98";
-    repo = "container-diffoscope";
-    tag = finalAttrs.version;
-    hash = "sha256-Ox5MfVzCQU8y+P3N5KqTEMSVLHeIaIhZlZdgLAdA2Mw=";
-  };
+  src = ./.;
+  # src = fetchFromGitHub {
+  #   owner = "kkignasiak98";
+  #   repo = "container-diffoscope";
+  #   tag = finalAttrs.version;
+  #   hash = "sha256-E1eQ5bmCOXh9BMRErBjOGzgbLr3iuSOR9VRzOklZkgM=";
+  # };
 
   build-system = [
-    python3Packages.poetry-core
+    python3Packages.uv-build
     python3Packages.pythonRelaxDepsHook
   ];
 
@@ -44,7 +45,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
   meta = {
     description = "Elegant Container filesystem comparison";
     homepage = "https://github.com/kkignasiak98/container-diffoscope";
-    changelog = "https://github.com/kkignasiak98/container-diffoscope/releases/tag/${finalAttrs.src.tag}";
+    changelog = "https://github.com/kkignasiak98/container-diffoscope/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit; 
     maintainers = with lib.maintainers; [kkignasiak98]; #there will be more in the future!
     mainProgram = "container-diffoscope";
